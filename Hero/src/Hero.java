2654a92220c9d95e78f7653fb06f6ffd97238eec
CZH-HW <2473464132@qq.com>
2019-07-28 09:30:07 +0800

    modify the codes

---
+++ Hero/src/Hero.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Hero {
+	
+	//public void method1(final int i) {
+	//}
+		
+	public static void main(String[] arg){	
+		
+		
+		
+		/*
+		int i = 1;
+		i+=++i;
+		System.out.println(i); //输出结果为3		
+		*/
+		
+		
+		/*
+		int i = 3; // 二进制是11
+		int j = 2; // 二进制是10
+		int c = ((i | j) ^ (i & j)) << 2 >>> 1;
+		System.out.println(c);
+		*/
+		
+			
+		/*
+		int i = 5;
+        String b = Integer.toBinaryString(i); // 十进制转换为二进制的表达
+        System.out.println(i+" 的二进制表达是: " + b);
+		*/		
+		
+		/*
+		Scanner compare = new Scanner(System.in);
+		int firstInt = compare.nextInt();
+		System.out.println("第一个整数：" + firstInt);
+		int secondInt = compare.nextInt();
+		System.out.println("第一个整数：" + secondInt);
+		System.out.println("比较"+firstInt+">"+secondInt+":"+(firstInt>secondInt));
+        System.out.println("比较"+firstInt+">="+secondInt+":"+(firstInt>=secondInt));
+        System.out.println("比较"+firstInt+"<"+secondInt+":"+(firstInt<secondInt));
+        System.out.println("比较"+firstInt+"<="+secondInt+":"+(firstInt<=secondInt));
+        System.out.println("比较"+firstInt+"=="+secondInt+":"+(firstInt==secondInt));
+        System.out.println("比较"+firstInt+"!="+secondInt+":"+(firstInt!=secondInt));
+		*/
+		
+		
+		/*
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println("第一个整数：" + a);
+		int b = s.nextInt();
+		System.out.println("第二个整数：" + b);
+		int sum1 = a + b;
+		System.out.println("两个整数之和：" + sum1);
+		 */
+		
+		
+		/*
+		int i = 1;
+		int j = ++i + i++ + ++i + ++i + i++;
+		System.out.println(j);
+		*/
+		
+		
+		/* 
+		Scanner sc=new Scanner(System.in);
+        System.out.println("请输入身高,单位为m");  
+        float height=sc.nextFloat();
+        System.out.println("请输入体重，单位为kg");
+        float weight=sc.nextFloat();
+        float BMI = (float)(weight/(height*height));
+        if(BMI<18.5)
+            System.out.println("体重过轻");
+        else if(BMI<24)
+            System.out.println("体重正常");
+        else if(BMI<27)
+            System.out.println("体重过重");
+        else if(BMI<30)
+            System.out.println("轻度肥胖");
+        else if(BMI<35)
+            System.out.println("中度肥胖");
+        else
+            System.out.println("重度肥胖");
+        sc.close();
+        */
+				
+		
+	}
+
+}
